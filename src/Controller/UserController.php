0cd58a940c20124075eb4ff81791a9f8c7879fdb
Dylan <dylan.hochet50@gmail.com>
2024-03-17 02:57:43 +0100

    Added a generic ExceptionListener to handle kernel errors

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,16 +12,9 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'create_user', methods: 'POST')]
+//    #[IsGranted('ROLE_USER')]
     public function createUser(Request $request): JsonResponse
     {
-        var_dump($request->request->all());
-        return $this->json(['test' => 'test POST']);
+        return $this->json(['apiToken' => 'my_token_abc']);
     }
-
-//    #[Route('/user', name: 'app_user', methods: 'GET')]
-//    #[IsGranted('ROLE_USER')]
-//    public function index(): JsonResponse
-//    {
-//        return $this->json(['test' => 'test GET']);
-//    }
 }
