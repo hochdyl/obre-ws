d36605c4f1304bf18250898e16a7d63a43ce4484
Dylan <dylan.hochet50@gmail.com>
2024-04-16 21:38:29 +0200

    Return an explicit message if body is missing from request

--- src/EventListener/ApiExceptionListener.php
+++ src/EventListener/ApiExceptionListener.php
@@ -5,6 +5,7 @@ namespace App\EventListener;
 use App\Service\ValidationService;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver\RequestPayloadValueResolver;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
@@ -27,14 +28,22 @@ final class ApiExceptionListener
         $type = 'message';
         $value = $exception->getMessage();
 
-        $previousException = $exception->getPrevious();
+        // The file where the exception was thrown
+        $filename = pathinfo($exception->getFile())['filename'];
 
-        if ($previousException instanceof ValidationFailedException) {
-            $violations = $previousException->getViolations();
+        // Check if exception come from RequestPayloadValueResolver
+        if ($filename === pathinfo(RequestPayloadValueResolver::class)['filename']) {
+            $value = 'Request body is missing or improperly formatted';
 
-            $status = 'fail';
-            $type = 'data';
-            $value = ValidationService::getViolations($violations);
+            // Check if exception come from ValidationFailedException
+            $previousException = $exception->getPrevious();
+            if ($previousException instanceof ValidationFailedException) {
+                $violations = $previousException->getViolations();
+
+                $status = 'fail';
+                $type = 'data';
+                $value = ValidationService::getViolations($violations);
+            }
         }
 
         $response = new JsonResponse([
