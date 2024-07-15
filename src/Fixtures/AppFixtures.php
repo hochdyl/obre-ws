06d09c919ed71cecf0199136174efcaccd059d68
Dylan <dylan.hochet50@gmail.com>
2024-07-15 20:13:12 +0200

    :bookmark: v1.0.1

--- src/Fixtures/AppFixtures.php
+++ src/Fixtures/AppFixtures.php
@@ -3,45 +3,22 @@
 namespace App\Fixtures;
 
 use App\Entity\AppVersion;
-use App\Entity\Game;
-use App\Entity\User;
-use App\Service\UserPasswordService;
-use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function __construct(private readonly UserPasswordService $userPasswordService){}
+    public function __construct(){}
 
     public function load(ObjectManager $manager): void
     {
         $version = new AppVersion();
         $version
             ->setName('Vanish')
-            ->setNumber("0.1.0");
+            ->setNumber("1.0.0")
+            ->setFeatures("* Obre release");
 
         $manager->persist($version);
-
-        $user = new User();
-        $user
-            ->setUsername('admin')
-            ->setPassword('admin');
-        $this->userPasswordService->hashPassword($user);
-        $user->generateSessionToken();
-
-        $manager->persist($user);
-
-        $game = new Game();
-        $game
-            ->setTitle("Fixture game")
-            ->setSlug('fixture-game')
-            ->setStartedAt(new DateTimeImmutable())
-            ->setGameMaster($user)
-            ->setCreator($user)
-            ->setClosed(false);
-
-        $manager->persist($game);
 
         $manager->flush();
     }
